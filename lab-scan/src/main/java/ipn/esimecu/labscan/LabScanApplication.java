3d9c0621b804f8bcec5b49f177b40246a694dc02
Mictlan <kevinw130501@gmail.com>
2023-11-22 00:58:58 -0600

    [added]: Spring boot project

---
+++ lab-scan/src/main/java/ipn/esimecu/labscan/LabScanApplication.java
@@ -0,0 +1,13 @@
+package ipn.esimecu.labscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabScanApplication.class, args);
+	}
+
+}
---
+++ lab-scan/src/test/java/ipn/esimecu/labscan/LabScanApplicationTests.java
@@ -0,0 +1,13 @@
+package ipn.esimecu.labscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
