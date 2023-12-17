94e3556a4873b19999a2fa087256c19cac902103
MaxAguilar <aaguilara1903@alumno.ipn.mx>
2023-12-17 00:03:33 -0600

    XLSX Service harcodeado

--- lab-scan/src/test/java/ipn/esimecu/labscan/services/XlsxServiceTest.java
+++ lab-scan/src/test/java/ipn/esimecu/labscan/services/XlsxServiceTest.java
@@ -1,7 +1,15 @@
 package ipn.esimecu.labscan.services;
 
+import ipn.esimecu.labscan.dto.Data;
+import ipn.esimecu.labscan.entity.AttendanceEntity;
+import ipn.esimecu.labscan.entity.LaboratoryEntity;
+import ipn.esimecu.labscan.entity.ScheduleEntity;
+import ipn.esimecu.labscan.entity.StudentEntity;
 import ipn.esimecu.labscan.service.XlsxService;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -25,7 +33,26 @@ public class XlsxServiceTest {
     @Test
     @Order(1)
     public void generateXlsxTest() {
-        Assertions.assertDoesNotThrow(() -> xlsxService.generateXlsx(null));
+        StudentEntity max = new StudentEntity();
+        LaboratoryEntity arturo = new LaboratoryEntity(); 
+        AttendanceEntity alan = new AttendanceEntity();
+        Data wil = new Data();
+        
+        max.setFullName("Max Aguilar");
+        arturo.setLaboratoryId(1);
+        max.setPcNumber("2");
+        max.setStudentId(14444444);
+        
+       wil.setObservation(new AttendanceEntity());
+       alan.setObservation("ninguna");
+    
+        wil.setLaboratory(arturo);
+        wil.setStudent(max);
+        wil.setAttendance(alan);
+       
+        Assertions.assertDoesNotThrow(() -> xlsxService.generateXlsx(Arrays.asList(wil)));
     }
+    
+    
 
 }
